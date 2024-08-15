88afee30a634ab005c3e634fe13f19a418923d7b
Ben Vidulich <ben.vidulich@medipass.io>
2024-08-15 16:53:17 +1000

    add routes with security bugs

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
 
-import keywords from './keywords.json';
+import keywords from "./keywords.json";
 
 export const app: express.Application = express();
 
@@ -12,11 +12,51 @@ if (process.env.CORS_HOST) {
 }
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.json({
-    "keywords_url": "/keywords"
+    keywords_url: "/keywords",
   });
 });
-app.get('/keywords', (req: Request, res: Response) => {
+
+app.get("/keywords", (req: Request, res: Response) => {
   res.json(keywords);
 });
+
+app.post("/keywords", (req: Request, res: Response) => {
+  const keyword = req.body;
+  if (!keyword.keyword) {
+    res.status(400);
+    res.json({ error: "invalid keyword" });
+    return;
+  }
+
+  // Danger, Will Robinson!
+  keywords.push(keyword);
+
+  res.json(keyword);
+});
+
+app.get("/keywords/:keyword", (req: Request, res: Response) => {
+  const keyword = req.params.keyword;
+
+  if (!keyword) {
+    notFound(res);
+    return;
+  }
+
+  const matches = keywords.filter((obj) => {
+    return obj.keyword === keyword;
+  });
+
+  if (matches.length === 0) {
+    notFound(res);
+    return;
+  }
+
+  return res.json(matches[0]);
+});
+
+const notFound = (res: Response) => {
+  res.status(404);
+  res.json({ error: "Not found" });
+};
